4057e52d9e29c81114766643032e7820372321f6
xgy221 <17302558812@163.com>
2018-04-17 19:07:05 +0800

    show

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     # 显示所有的主题
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
+
+    # 特定主题的详细页面
+    path('topics/<int:topic_id>', views.topic, name='topic')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -13,3 +13,11 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """显示单个主题及其所有条目"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
