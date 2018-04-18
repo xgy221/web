7ab406a3502f0a526a991aaec8375d65b1dcff3d
xgy221 <17302558812@163.com>
2018-04-18 14:17:45 +0800

    update

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -17,5 +17,8 @@ urlpatterns = [
     path('topics/<int:topic_id>', views.topic, name='topic'),
 
     # 用于添加新主题的网页
-    path('new_topic/', views.new_topic, name='new_topic')
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    # 用于添加新条目的页面
+    path('new_entry/<int:topic_id>', views.new_entry, name='new_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -40,3 +40,23 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """在特定的主题中添加新条目"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # 未提交数据，创建一个空表单
+        form = EntryForm()
+    else:
+        # POST提交的数据，对数据进行处理
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
