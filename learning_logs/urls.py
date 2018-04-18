02ca7ca5689bcc3356c9be30bdab4e51ba70bd3f
xgy221 <17302558812@163.com>
2018-04-18 10:54:05 +0800

    form

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -14,5 +14,8 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
 
     # 特定主题的详细页面
-    path('topics/<int:topic_id>', views.topic, name='topic')
+    path('topics/<int:topic_id>', views.topic, name='topic'),
+
+    # 用于添加新主题的网页
+    path('new_topic/', views.new_topic, name='new_topic')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Topic
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
@@ -21,3 +24,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """添加新主题"""
+    if request.method != 'POST':
+        # 未提交数据，创建一个新表单
+        form = TopicForm()
+    else:
+        # POST提交的数据，对数据进行处理
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
