e482eefeaf2bb89de3948bbe9fa9d6820650c439
xgy <2406753528@qq.com>
2018-04-24 10:41:45 +0800

    Heroku

--- web/settings.py
+++ web/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
 
     # 第三方应用程序
     'bootstrap3',
-    
+
     # 我的应用程序
     'learning_logs',
     'users',
@@ -126,3 +126,22 @@ LOGIN_URL = '/users/login/'
 BOOTSTRAP3 = {
     'include_jquery': True,
 }
+
+# Heroku设置
+if os.getcwd() == '/app':
+    import dj_database_url
+
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+
+    # 让request.is_secure承认X-FORWARDED_Proto头
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # 支持所有的主机头（host header）
+    ALLOWED_HOSTS = ['*']
+
+    # 静态资产配置
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
